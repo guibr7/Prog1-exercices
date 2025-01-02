986be68080d32893542a068f54f2ea9f80ecd29e
Guilherme Henrique <guilhermehenrique33@outlook.com.br>
2025-01-02 17:19:36 -0300

    Add aula pratica 3

---
+++ aulaPratica03/ex06.c
@@ -0,0 +1,23 @@
+//Guilherme Henrique Silva Miranda 24.2.4090
+#include <stdio.h>
+void converterDistancias(float metros, float *jardas, float *pes, float *polegadas);
+void converterDistancias(float metros, float *jardas, float *pes, float *polegadas){
+    *jardas = metros*1.094;
+    *pes = metros*3.281;
+    *polegadas = metros*39.3701;
+}
+int main(){
+float metros,jardas,pes,polegadas;
+float *j,*pe,*pol;
+j = &jardas;
+pe = &pes;
+pol = &polegadas;
+printf("Digite o valor em Metros: ");
+scanf("%f",&metros);
+printf("\n\n");
+converterDistancias(metros,j,pe,pol);
+printf("Jardas: %.3f\n\n",jardas);
+printf("Pés: %.3f\n\n",pes);
+printf("Polegadas: %.3f\n",polegadas);
+return 0;
+}
