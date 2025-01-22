e19a453f4ae9fdb6b84a036a447ce5d6c37f4cb7
Guilherme Henrique <guilhermehenrique33@outlook.com.br>
2025-01-22 14:07:11 -0300

    Exercício 1 - aula 05

---
+++ aulaPratica05/ex01.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void divisores(int n);
+void divisores(int n){
+    int i = 1;
+    int r;
+    printf("Os divisores de %d são: ",n);
+    while(i <= n){
+        r = n%i;
+        if(r == 0){
+            printf("%d ",i);
+        };
+        i++;
+    };
+}
+int main(){
+    int n;
+    printf("Digite o valor de n: ");
+    scanf("%d",&n);
+    printf("\n");
+    divisores(n);
+    return 0;
+}
