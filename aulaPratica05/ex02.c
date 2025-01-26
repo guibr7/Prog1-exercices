9924c267753aaf9a1fe639f25771451b63dd6570
Guilherme Henrique <guilhermehenrique33@outlook.com.br>
2025-01-26 02:48:07 -0300

    Exercício 3 - aula 5

--- aulaPratica05/ex02.c
+++ aulaPratica05/ex02.c
@@ -1,9 +1,10 @@
-//Guilherme Henrique Silva Miranda 24.2.4090
+/*Guilherme Henrique Silva Miranda 24.2.4090*/
 #include <stdio.h>
 int quadrado_por_soma(int n);
 int quadrado_por_soma(int n){
    int contadorImpar,i,resultado;
    i = 1;
+   resultado = 0;
    contadorImpar = 1;
    while(contadorImpar <= n){
     if(i%2 != 0){
--- aulaPratica05/ex03.c
+++ aulaPratica05/ex03.c
@@ -1,5 +1,14 @@
 //Guilherme Henrique Silva Miranda 24.2.4090
 #include <stdio.h>
 int main(){
-    
+    float nota1,nota2,media;
+        printf("Digite as notas do aluno: ");
+        scanf("%f %f",&nota1,&nota2);
+        while(nota1 >= 0 && nota2 >= 0){
+             media = (nota1 + nota2)/2.0;
+             printf("A média das notas é %.1f\n",media);
+             printf("Digite as notas do aluno: ");
+             scanf("%f %f",&nota1,&nota2);
+            }; 
+return 0;
 }
