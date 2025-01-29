c65208560f4b3ef11fd3cc0354022f79b0873fbe
Guilherme Henrique <guilhermehenrique33@outlook.com.br>
2025-01-29 14:32:47 -0300

    Exercicio 2 - aula 6

---
+++ aulaPratica06/ex02.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+    float n1,resultado;
+    float soma = 0;
+    int cont = 1;
+    for(int i = 0; cont == 1; i++ ){
+        printf("Digite um valor: ");
+        scanf("%f",&n1);
+        if(n1 == 0 && i == 0){
+            cont = 0;
+            printf("Não foi possível calcular MA!\n");
+        }
+        if(n1 != 0){
+            soma+=n1;
+        }
+
+        if(n1 == 0 && i != 0){
+            resultado  = (1.0/i)*soma;
+            printf("MA = %.2f\n",resultado);
+            break;
+        }
+    }
+ return 0;
+}
